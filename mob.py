f8fc6b515e3a4668c39191c49d3d78f924076fc1
Aareon Sullivan <askully13@gmail.com>
2020-09-16 16:17:54 -0500

    Update mob.py

--- mob.py
+++ mob.py
@@ -1,29 +1,35 @@
+
+import configparser
+import os
+import pprint
+import sys
+from time import time
+
 try:
     import pygame_sdl2
     pygame_sdl2.import_as_pygame()
-    import sys
-    import os
-    #from pygame.locals import *
-    from time import time
-    import pprint
-    from loader import load_png
-    import configparser, os
-except ImportError as err:
-    print("couldn't load module. %s" % (err))
-    sys.exit(2)
+except ImportError:
+    try: import pygame as pygame_sdl2
+    except ImportError:
+        raise
+
+from loader import load_png
+
 
 class Mob(pygame_sdl2.sprite.Sprite):
     """ Mob class, generic class for a sprite """
-    def __init__(self, position, tw):
+    def __init__(self, position, tw, name=None):
         self.tw = tw
         config = configparser.ConfigParser()
-        config.read_file(open('mob.cfg'))
+        with open('mob.cfg') as f:
+            config.read_file(f)
+
         imageset = 'imagename' + str(self.tw)
         imagename = config.get('mob', imageset)
         self.image, self.rect = load_png(imagename)
         self.position = position
         self.miningskill = 5
-        self.name = "Jimmy"
+        self.name = name or "Jimmy"
         self.surface = pygame_sdl2.transform.scale(self.image, (self.tw, self.tw))
         self.pathlines = []
         self.job = None
